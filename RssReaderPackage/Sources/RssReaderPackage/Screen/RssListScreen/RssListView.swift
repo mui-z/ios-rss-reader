be108ee025014b988f93ca90d85cc8c7982e536c
mui-z <93278577+mui-z@users.noreply.github.com>
2023-01-07 11:41:58 +0900

    add: add list cell

--- RssReaderPackage/Sources/RssReaderPackage/Screen/RssListScreen/RssListView.swift
+++ RssReaderPackage/Sources/RssReaderPackage/Screen/RssListScreen/RssListView.swift
@@ -1,19 +1,28 @@
 import SwiftUI
 
 struct RssListView: View {
-    @StateObject var viewModel: RssListViewModel
-
-    var body: some View {
+	@StateObject var viewModel: RssListViewModel
+	
+	var body: some View {
 		List {
-			HStack {
-				Text("ios new site")
-				Spacer()
-			}
-			HStack {
-				Text("ios new site")
-				Spacer()
-			}
+			rssCell
+			rssCell
+			rssCell
 		}
-    }
+	}
 }
 
+private extension RssListView {
+	
+	var rssCell: some View {
+		VStack(alignment: .leading) {
+			Text("iOS News Site")
+				.padding(.trailing)
+				.padding(.bottom, 1)
+			
+			Text("2023/1/7")
+				.font(.caption)
+				.foregroundColor(.gray)
+		}
+	}
+}
