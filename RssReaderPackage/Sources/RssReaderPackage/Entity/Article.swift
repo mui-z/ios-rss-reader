9b3301bdb02fbf2160140c4eae16e76f1182b5a5
mui-z <93278577+mui-z@users.noreply.github.com>
2023-01-07 11:48:52 +0900

    add: add entity

---
+++ RssReaderPackage/Sources/RssReaderPackage/Entity/Article.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by osushi on 2023/01/07.
+//
+
+import Foundation
+
+struct Article {
+	let title: String
+	let url: URL
+}
---
+++ RssReaderPackage/Sources/RssReaderPackage/Entity/WebSite.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by osushi on 2023/01/07.
+//
+
+import Foundation
+
+struct WebSite {
+	let title: String
+	let rssUrl: URL
+	let lastUpdateDate: Date
+}
